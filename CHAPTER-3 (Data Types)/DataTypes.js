237957da233b257af64f93f7630bd4ae44f9931a
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-27 23:02:56 +0530

    JS

--- CHAPTER-3 (Data Types)/DataTypes.js
+++ CHAPTER-3 (Data Types)/DataTypes.js
@@ -1,6 +1,20 @@
 //Primitive
 //7 types:String,Number.boolean,null,undefined,symbol,
 //BigInt
+const score=100;
+const scoreValue=100.3
+const isLoggedIn=false;
+const outSideTemp=null;
+let userEmail;
+
+const ig=Symbol('123');
+const anotherId=Symbol('123');
+const bigNumber=3242332497345733293809709n;
 
 //Reference(Non-Primitive)
 //Array,Objects,Functions
+const hero=["Shaktiman","Naagraj","Doga"];
+let obj={
+    name:"Ritish",
+    age:21,
+}
