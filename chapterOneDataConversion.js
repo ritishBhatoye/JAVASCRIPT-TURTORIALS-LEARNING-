06e43c35d1edfb165c961024d2189e104fb156b6
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-27 07:29:36 +0530

    JS CH-1

---
+++ chapterOneDataConversion.js
@@ -0,0 +1,26 @@
+let score=33;
+console.log("TYPE OF SCORE :- "+typeof(score));
+console.log("TYPE OF")
+let a="10";
+console.log("TYPE OF a :- "+typeof(a));
+let value=Number(a);
+console.log("TYPE OF a :- "+typeof(value));
+//if
+let val="10asc";
+let ans=Number(val);
+console.log(typeof(ans));
+console.log(ans);
+//"10"=>33
+//"10asc"=>NaN
+//true=>1;false=>0
+let isLoggedIn=1;
+let ni=Boolean(isLoggedIn);
+console.log(ni);
+//1=>true
+//0=>false
+//""=>false
+//"hitesh"=>true
+let someNumber=33;
+let StringSN=String(someNumber);
+console.log(StringSN);
+console.log(typeof(StringSN));
