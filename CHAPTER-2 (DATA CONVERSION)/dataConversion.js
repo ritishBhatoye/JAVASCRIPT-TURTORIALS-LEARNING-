6fc6bbf607b89ccf18a2b7cd013f91cb0b25c4fd
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-27 07:48:14 +0530

    js ch-2

---
+++ CHAPTER-2 (DATA CONVERSION)/dataConversion.js
@@ -0,0 +1,28 @@
+//********OPERATIONS*******//
+let val=3;
+let negVal=-val;
+console.log(negVal);
+//ARTHIMATIC
+console.log(10+2);
+console.log(10-2);
+console.log(10*2)
+console.log(10**3);
+console.log(10/3);
+console.log(10%3);
+
+let s1="HELLO";
+let s2="GEEKS";
+
+let s3=s1+" "+s2;
+console.log(s3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+let count=100;
+console.log(++count);
+console.log(count++);
