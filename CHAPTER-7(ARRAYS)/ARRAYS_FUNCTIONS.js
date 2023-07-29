81f88b596f0c1e0c65919364b80774493edbcf55
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-29 09:16:09 +0530

    JS

--- CHAPTER-7(ARRAYS)/ARRAYS_FUNCTIONS.js
+++ CHAPTER-7(ARRAYS)/ARRAYS_FUNCTIONS.js
@@ -3,5 +3,12 @@ var a2=[5,6,7,8];
 // var c=a1.concat(a2);
 // console.log(c);
 //SIMILIAR TO CONCAT
-const sprd=[...a1,...a2];
-console.log(sprd);
+// const sprd=[...a1,...a2];
+// console.log(sprd)
+// const arr=[1,2,3,[42,21],4,5,6,[7,[30,25,9]]];
+// console.log(arr.flat(Infinity));
+// console.log(Array.from("RITISH"));
+let s1=100;
+let s2=200;
+let s3=300;
+console.log(Array.of(s1,s2,s3));
