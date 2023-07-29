c6d55f5240d8c43eee1b7018bb90f566b268bb07
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-29 09:00:48 +0530

    JS

--- CHAPTER-7(ARRAYS)/Array.js
+++ CHAPTER-7(ARRAYS)/Array.js
@@ -1,5 +1,13 @@
 //ARRAYS
-const myArr=[2,4,6,8];
-for(let i=0;i<myArr.length;i++){
-    console.log(myArr[i]);
+const myArr=[2,4,6,8,10];
+// for(let i=0;i<myArr.length;i++){
+//     console.log(myArr[i]);
+// }
+console.log(myArr);
+myArr.push(9);
+if(myArr.includes(9)){
+    console.log("NINE IS PRESENT IN AN ARRAY ");
+}
+else{
+    console.log("NINE IS NOT PRESENT IN AN ARRAY ");
 }
