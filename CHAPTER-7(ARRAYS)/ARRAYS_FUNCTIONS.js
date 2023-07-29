ff478af4cf7904cbb3169d674338e5b3d5925ed5
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-29 09:04:47 +0530

    JS

--- CHAPTER-7(ARRAYS)/ARRAYS_FUNCTIONS.js
+++ CHAPTER-7(ARRAYS)/ARRAYS_FUNCTIONS.js
@@ -1,4 +1,7 @@
 var a1=[1,2,3,4];
 var a2=[5,6,7,8];
-var c=a1.concat(a2);
-console.log(c);
+// var c=a1.concat(a2);
+// console.log(c);
+//SIMILIAR TO CONCAT
+const sprd=[...a1,...a2];
+console.log(sprd);
