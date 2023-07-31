085e640dcb9e408e77f681df8dc6b1840f0880fa
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-31 16:43:25 +0530

    JS

--- CHAPTER-10(HIGH ORDER ARRAY LOOPS)/four.js
+++ CHAPTER-10(HIGH ORDER ARRAY LOOPS)/four.js
@@ -1,15 +1,17 @@
-const o={
-js:'JavaScript',
-cpp:'C++',
-rb:'ruby',
-swift:'swift by APPLE'
-}
+// const o={
+// js:'JavaScript',
+// cpp:'C++',
+// rb:'ruby',
+// swift:'swift by APPLE'
+// }
 
-for(const k in o){
-    console.log(`${k} : ${o[k]}`);
-}
+// for(const k in o){
+//     console.log(`${k} : ${o[k]}`);
+// }
 
-const prog=["JS","C++","JAVA","PYTHON","R","RUBY"];
-for(const k in prog){
-    console.log(prog[k]);
-}
+// const prog=["JS","C++","JAVA","PYTHON","R","RUBY"];
+// for(const k in prog){
+//     console.log(prog[k]);
+// }
+const c=["JS","RUBY","JAVA","PYTHON","CPP"];
+c.forEach((i)=>console.log(i));
