2b5dc945e39b322c46e94879c6f6ced0b5cef71a
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-31 16:35:58 +0530

    JS

--- CHAPTER-10(HIGH ORDER ARRAY LOOPS)/HighOrderArrayLoops.js
+++ CHAPTER-10(HIGH ORDER ARRAY LOOPS)/HighOrderArrayLoops.js
@@ -1,4 +1,4 @@
-//FOROF
+//FOR in
 const arr=[1,2,3,4,5];
 for(const i in arr){
     console.log(arr[i]);
@@ -8,4 +8,16 @@ const m=new Map();
 m.set('IN','India')
 m.set('USA','United States Of America')
 m.set('Fr','France')
-console.log(m);
+//console.log(m);
+for(const [k,v] of m){
+    console.log(k, ':-' ,v);
+}
+
+const o={
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+
+for(const [k,v] of o){
+    console.log(k, ":-" ,v);
+}
---
+++ CHAPTER-10(HIGH ORDER ARRAY LOOPS)/four.js
@@ -0,0 +1,15 @@
+const o={
+js:'JavaScript',
+cpp:'C++',
+rb:'ruby',
+swift:'swift by APPLE'
+}
+
+for(const k in o){
+    console.log(`${k} : ${o[k]}`);
+}
+
+const prog=["JS","C++","JAVA","PYTHON","R","RUBY"];
+for(const k in prog){
+    console.log(prog[k]);
+}
