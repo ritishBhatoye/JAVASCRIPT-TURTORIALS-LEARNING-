b66978a2de15f2868669a4b3fedd52b3d7c5a0b2
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-31 16:57:56 +0530

    JS

--- CHAPTER-10(HIGH ORDER ARRAY LOOPS)/four.js
+++ CHAPTER-10(HIGH ORDER ARRAY LOOPS)/four.js
@@ -14,4 +14,22 @@
 //     console.log(prog[k]);
 // }
 const c=["JS","RUBY","JAVA","PYTHON","CPP"];
-c.forEach((i)=>console.log(i));
+//c.forEach((i)=>console.log(i));
+// c.forEach((i,index,c)=>{
+//     console.log(i,index,c)
+// })
+const code=[
+    {
+        languageName:"Javascript",
+        languageFileName:"JS"
+    },
+    {
+        languageName:"Python",
+        languageFileName:"PY",
+    },
+    {
+        languageName:"C++",
+        languageFileName:"CPP"
+    }
+]
+code.forEach((i)=>{console.log(`${i.languageName} :- ${i.languageFileName}`)})
