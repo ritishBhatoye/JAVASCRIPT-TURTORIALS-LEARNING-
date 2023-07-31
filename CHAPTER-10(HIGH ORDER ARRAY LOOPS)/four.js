fe264b6828dc95894fb2ab1e4926f419b0a05ce9
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-31 22:40:20 +0530

    JS

--- CHAPTER-10(HIGH ORDER ARRAY LOOPS)/four.js
+++ CHAPTER-10(HIGH ORDER ARRAY LOOPS)/four.js
@@ -13,24 +13,42 @@
 // for(const k in prog){
 //     console.log(prog[k]);
 // }
-const c=["JS","RUBY","JAVA","PYTHON","CPP"];
-//c.forEach((i)=>console.log(i));
-// c.forEach((i,index,c)=>{
-//     console.log(i,index,c)
-// })
-//FOR EACH FOR LOOPING INTO OBJECT
-const code=[
-    {
-        languageName:"Javascript",
-        languageFileName:"JS"
-    },
-    {
-        languageName:"Python",
-        languageFileName:"PY",
-    },
-    {
-        languageName:"C++",
-        languageFileName:"CPP"
-    }
-]
-code.forEach((i)=>{console.log(`${i.languageName} :- ${i.languageFileName}`)})
+// const c=["JS","RUBY","JAVA","PYTHON","CPP"];
+// //c.forEach((i)=>console.log(i));
+// // c.forEach((i,index,c)=>{
+// //     console.log(i,index,c)
+// // })
+// //FOR EACH FOR LOOPING INTO OBJECT
+// const code=[
+//     {
+//         languageName:"Javascript",
+//         languageFileName:"JS"
+//     },
+//     {
+//         languageName:"Python",
+//         languageFileName:"PY",
+//     },
+//     {
+//         languageName:"C++",
+//         languageFileName:"CPP"
+//     }
+// ]
+// code.forEach((i)=>{console.log(`${i.languageName} :- ${i.languageFileName}`)})
+
+// //FILTER:- It is quite similiar to other LOOPING METHOD the only difference is that it requires the CONDITION for printing
+//const myNums=[1,2,3,4,5,6,7,8];
+
+// const N=myNums.filter((n)=>n>4);
+// console.log(N)
+// const n=[1,2,3,4,5,6,7,8,9,10]
+
+// const N=n
+//          .map((num)=>num*10)
+//          .map((num)=>num+1)
+//          .filter((num)=> num>=40)
+//  console.log(N)
+const N=[1,2,3]
+const Sum=N.reduce((a,c)=>{
+    return a+c
+},0)
+ console.log(Sum)
