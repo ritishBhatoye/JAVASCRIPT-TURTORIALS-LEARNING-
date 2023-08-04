c23e16c330957d8f09fc4e2c333a2b839673c7f5
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-04 16:42:45 +0530

    JS

---
+++ CHAPTER-12/index.js
@@ -0,0 +1,9 @@
+document.querySelector('#grandparent').addEventListener('click',function(){
+    console.log("Grandparent Clicked!!!")
+})
+document.querySelector('#parent').addEventListener('click',function(){
+    console.log("Parent Clicked!!")
+})
+document.querySelector('#child').addEventListener('click',function(){
+    console.log("Child Clicked!")
+})
