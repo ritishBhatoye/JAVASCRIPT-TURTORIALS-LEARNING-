b9988727adf5ca1a7b35633621e52d1709c3a4c5
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-04 16:52:54 +0530

    JS

--- CHAPTER-12/index.js
+++ CHAPTER-12/index.js
@@ -1,9 +1,9 @@
 document.querySelector('#grandparent').addEventListener('click',function(){
     console.log("Grandparent Clicked!!!")
-})
+},true)
 document.querySelector('#parent').addEventListener('click',function(){
     console.log("Parent Clicked!!")
-})
+},true)
 document.querySelector('#child').addEventListener('click',function(){
     console.log("Child Clicked!")
-})
+},true)
